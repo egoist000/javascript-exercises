c3cb51448240865e95cc5f00292015219093e0c3
Lorenzo Epifani <lor.epifani@stud.uniroma3.it>
2021-10-03 23:14:12 +0200

    removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    const array = args[0];
+    const resultArray = [];
+    array.forEach(element => {
+        if(!args.includes(element, 1)) {
+            resultArray.push(element);
+        }
+    });
+    return resultArray;
 };
 
 // Do not edit below this line
