f862b1c0d69d7744c20cacf726335f9d085f20ef
Lorenzo Epifani <lor.epifani@stud.uniroma3.it>
2021-10-03 22:08:48 +0200

    helloWorld, repeatString, reverseString done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
 const reverseString = function(str) {
-
+    let chars = str.split("");
+    let tmp = [];
+    for(let i = chars.length - 1; i >= 0; i--) {
+        tmp.push(chars[i]);
+    }
+    return tmp.join("");
 };
 
 // Do not edit below this line
