b1b8e9b6b0a28b823dbc5fd6d0c80825942f1dae
Lorenzo Epifani <lor.epifani@stud.uniroma3.it>
2021-10-04 16:32:17 +0200

    sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, secondNum) {
+    let sum = 0;
+    if(!Number.isInteger(firstNum) || !Number.isInteger(secondNum)) {
+        return "ERROR";
+    }
+    if(firstNum < 0 || secondNum < 0) {
+        return "ERROR";
+    }
+    if(firstNum < secondNum) {
+        for(let i = firstNum; i <= secondNum; i++) {
+            sum += i;
+        }
+    }
+    else {
+        for(let i = firstNum; i >= secondNum; i--) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
