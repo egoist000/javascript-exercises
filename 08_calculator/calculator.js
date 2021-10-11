7f176985d6fde5e6c5d519b51e1ce54628240410
Lorenzo Epifani <lor.epifani@stud.uniroma3.it>
2021-10-11 22:34:22 +0200

    calculator, palindromes, fibonacci

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+  array.forEach(e => {
+    sum += e;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let mul = 1;
+  array.forEach(e => {
+    mul *= e;
+  });
+  return (array.length > 1) ? mul : 0;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if(a === 0 || a === 1) {
+    return 1;
+  }
+  return a * factorial(a - 1);
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    const string = text.toLowerCase().replace(/[^A-Za-z]/g, "");
+    return string.split("").reverse().join("") == string;
 };
 
 // Do not edit below this line
