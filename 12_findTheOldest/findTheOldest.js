2ee400f59c9f8d5915fa739302841483aceca8c3
Lorenzo <lor.epifani@stud.uniroma3.it>
2021-10-13 22:22:39 +0200

    getTheTitles, findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.reduce((oldestPerson, currentPerson) => {
+      const oldestPersonAge = calcAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath)
+      const currentAge = calcAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath)
+      return oldestPersonAge < currentAge ? currentPerson : oldestPerson
+    })
+};
+  
+const calcAge = function(birth, death) {
+    if (!death) {
+      death = new Date().getFullYear();
+    }
+    return death - birth;
 };
 
 // Do not edit below this line
